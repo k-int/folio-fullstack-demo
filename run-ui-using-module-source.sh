02b7eb285d0b91352281b4bc46be4b6b92445a88
Marc Johnson <marc@k-int.co.uk>
2017-03-20 12:37:53 +0000

    Remove trivial and okapi-console ui modules

--- run-ui-using-module-source.sh
+++ run-ui-using-module-source.sh
@@ -5,15 +5,9 @@ cd ../ui-users
 yarn unlink
 yarn link
 
-cd ../ui-okapi-console
-
-yarn unlink
-yarn link
-
 cd ../folio-fullstack-demo
 
 yarn link "@folio/users"
-yarn link "@folio-sample-modules/okapi-console"
 
 yarn install
 yarn start
--- stripes.config.js
+++ stripes.config.js
@@ -2,8 +2,6 @@ module.exports = {
   okapi: { 'url':'http://localhost:9130', 'tenant':'diku' },
   config: { reduxLog: true, disableAuth: true },
   modules: {
-    "@folio/users": {},
-    "@folio/trivial": {},
-    "@folio-sample-modules/okapi-console": {}
+    "@folio/users": {}
   }
 };
